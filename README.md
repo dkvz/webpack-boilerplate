61b43016f9615d648fadd925b01314e00f8fda0c
DkVZ <bouc@clanz.be>
2018-10-17 23:23:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Webpack Boilerplate
+See sections below for info about how to add specific elements to the project.
+
+## General CSS
+
+## SASS
+
+## Babel
+
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const contentBase = path.join(__dirname, 'dist');
+
+// Generates config objects for HtmlWebpackPlugin instances:
+const minifyOptions = {
+  removeComments: true,
+  collapseWhitespace: true,
+  removeAttributeQuotes: false
+}
+
+const config = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    path: contentBase,
+    publicPath: '/',
+    filename: '[name][hash:5].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)(\?.*)?$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'static/[name][hash:7].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pdf$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1,
+              name: 'static/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'static/'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: 'webroot', to: '' }
+    ])
+  ],
+  devServer: {
+    contentBase: contentBase,
+    historyApiFallback: true,
+    publicPath: '/',
+    port: 8081
+  }
+};
+
+// I have to do this export a function thingy just because
+// I need to determine the environment, and NODE_ENV is not
+// just unreliable, it's NOT WORKING AT ALL.
+module.exports = (env, argv) => {
+
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+
+  // Add the main index page here:
+  config.plugins.push(
+    new HtmlPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: (argv.mode === 'production') ? minifyOptions : false
+    })
+  );
+  
+  return config;
+};
